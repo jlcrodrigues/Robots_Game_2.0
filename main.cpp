0e8a42f3c84963efc4072c886e16c0879952a968
th0rz05 <73318820+th0rz05@users.noreply.github.com>
2021-05-18 23:27:42 +0100

    gitignore

--- main.cpp
+++ main.cpp
@@ -9,16 +9,16 @@ using namespace std;
 int main(){
     
     Game game("xxxx.txt");
-    //Maze maze(10,20);
-    //Player player(1,2,'H');
-    //Post post(1,2,'*');
-    //Robot robot(1,2);
+    Maze maze(10,20);
+    Player player(1,2,'H');
+    Post post(1,2,'*');
+    Robot robot(1,2);
 
     game.test();
-    //maze.test();
-    //player.test();
-    //post.test();
-    //robot.test();
+    maze.test();
+    player.test();
+    post.test();
+    robot.test();
 
     return 0;
 }
