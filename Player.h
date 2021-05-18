c6a3d0d7d74e395780ef494cb6cb2464f3ee47ef
José Luís Cunha Rodrigues <joseluiscunharodrigues@gmail.com>
2021-05-18 16:16:19 +0100

    Cala-te Tiago

---
+++ Player.h
@@ -0,0 +1,16 @@
+class Player
+{
+public:
+   Player(int row, int col, char symbol);
+   int getRow() const;
+   int getCol() const;
+   char getSymbol() const;
+   bool isAlive() const;
+   void setAsDead();
+   bool move(Movement delta);
+
+private:
+   int row, col;
+   bool alive;
+   char symbol;
+};
